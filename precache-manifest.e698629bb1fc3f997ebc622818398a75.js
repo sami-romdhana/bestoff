ed77930c947da2d5efc46336f756ab7cd1210d71
Sami Romdhana <sami.romdhana@outlook.com>
2020-09-05 15:11:32 +0200

    Updates

---
+++ precache-manifest.e698629bb1fc3f997ebc622818398a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e144e7414a7417cecdd9f836e8fe5336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d269070999e60595b85d",
+    "url": "/static/css/main.29457b76.chunk.css"
+  },
+  {
+    "revision": "e6acf4fe64aa62284cf3",
+    "url": "/static/js/2.4e794419.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.4e794419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d269070999e60595b85d",
+    "url": "/static/js/main.2d9380e4.chunk.js"
+  },
+  {
+    "revision": "3ae3a3db505cd6cf5d47",
+    "url": "/static/js/runtime-main.3d11393d.js"
+  },
+  {
+    "revision": "1d2a78056260f3a9a8e9c2233b81a2af",
+    "url": "/static/media/logo.1d2a7805.svg"
+  }
+]);
